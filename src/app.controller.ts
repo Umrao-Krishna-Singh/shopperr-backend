eda275e6d06fb1bde00e766eb099d5522c7ba5f0
Umrao-Krishna-Singh <umraokrishnasingh2@gmail.com>
2024-04-11 15:30:43 +0530

    new test post route

--- src/app.controller.ts
+++ src/app.controller.ts
@@ -1,4 +1,5 @@
-import { Controller, Get } from '@nestjs/common';
+import { Controller, Get, Post, Req } from '@nestjs/common';
+import { Request } from 'express';
 import { AppService } from './app.service';
 
 @Controller()
@@ -8,5 +9,11 @@ export class AppController {
   @Get('/myroute')
   getHello(): string {
     return this.appService.getHello();
+  }
+
+  @Post('/myroute')
+  printVal(@Req() request: Request): string {
+    console.log('received data', request.hostname);
+    return 'done';
   }
 }
