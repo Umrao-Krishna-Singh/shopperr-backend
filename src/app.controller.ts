a974a5d26890ef23f349909766ffc66cb4d56b71
Umrao-Krishna-Singh <umraokrishnasingh2@gmail.com>
2024-07-21 04:48:29 +0530

    update app controller

--- src/app.controller.ts
+++ src/app.controller.ts
@@ -1,6 +1,7 @@
 import { Controller, Get, Post, Req } from '@nestjs/common';
 import { Request } from 'express';
 import { AppService } from './app.service';
+import { z } from 'zod';
 
 @Controller()
 export class AppController {
@@ -12,8 +13,18 @@ export class AppController {
   }
 
   @Post('/myroute')
-  printVal(@Req() request: Request): string {
-    console.log(request.body);
+  printVal(@Req() req: Request): string {
+    const User = z.object({
+      firstName: z.string(),
+      lastName: z.string(),
+    });
+
+    const { success, data, error } = User.safeParse(req.body);
+
+    // extract the inferred type
+    type User = z.infer<typeof User>;
+
+    console.log(req.body);
     return 'done';
   }
 }
