e6b9c7c1c78707d448dcec7a8e8bcec8515ac731
Umrao-Krishna-Singh <umraokrishnasingh2@gmail.com>
2024-08-06 00:20:09 +0530

    kysley types, will need to update it later

--- prisma/keysley/types.ts
+++ prisma/keysley/types.ts
@@ -1,41 +1,47 @@
-import type { ColumnType } from 'kysely';
+import type { ColumnType } from 'kysely'
 export type Generated<T> =
   T extends ColumnType<infer S, infer I, infer U>
     ? ColumnType<S, I | undefined, U>
-    : ColumnType<T, T | undefined, T>;
-export type Timestamp = ColumnType<Date, Date | string, Date | string>;
+    : ColumnType<T, T | undefined, T>
+export type Timestamp = ColumnType<Date, Date | string, Date | string>
 
 export type Post = {
-  id: Generated<number>;
+  id: Generated<number>
   /**
    * @zod.string.max(126)
    */
-  title: string;
-  content: string;
-  created_at: Generated<Timestamp>;
-  updated_at: Timestamp;
-  createdById: number;
+  title: string
+  content: string
+  created_at: Generated<Timestamp>
+  updated_at: Timestamp
+  createdById: number
   /**
    * @zod.string.max(50)
    */
-  timezone: string;
-};
+  timezone: string
+}
+
 export type User = {
-  id: Generated<number>;
+  id: Generated<number>
   /**
    * @zod.string.max(80)
    */
-  username: string;
+  username: string
   /**
    * @zod.string.max(126)
    */
-  fullname: string;
+  fullname: string
   /**
    * @zod.string.max(256)
    */
-  email: string | null;
-};
+  email: string | null
+}
+
+// UPDATE THIS LIST! KYSLEY docs explain that these table types should not be used directly, instead use the wrappers provided
+// that will ensure that get, insert, update operations are handled well. For now this is okay, but update this later.
+// Will also have to test if prisma kysley zod package can even export those types properly or not
+
 export type DB = {
-  Post: Post;
-  User: User;
-};
+  Post: Post
+  User: User
+}
